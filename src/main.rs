0cb4c4df27b5b2bcd7b490e506a2a56e0dd39629
Ibis Sunrosa <drsunrosa@gmail.com>
2022-08-30 10:59:03 -0500

    Semi-working letter checker

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use colored::{self, Colorize};
+use rand::seq::SliceRandom;
+
+fn main() {
+    wordle();
+}
+
+/// Correctness of a letter used in a position.
+enum Correctness {
+    /// The correct letter is in the correct position.
+    Correct,
+
+    /// A correct letter is in an incorrect position.
+    CorrectLetter,
+
+    /// An incorrect (and unused) letter is used in an incorrect position.
+    Incorrect,
+}
+
+/// Whether a letter is used or not in the word to guessw.
+enum LetterUsage {
+    /// The letter is used in the word.
+    Used,
+
+    /// The letter is not used in the word.
+    Unused,
+}
+
+/// Result of the game that was played.
+enum GameResult {
+    /// The player won.
+    Success,
+
+    /// The player lost.
+    Failure,
+}
+
+/// Wordle configuration.
+struct Configuration {
+    /// Number of guess tries before the game is over.
+    guess_tries: u32,
+
+    /// Number of letters in the word to be guessed.
+    guess_letters: u8,
+}
+
+fn wordle() {
+    // Define program config
+    let config = Configuration {
+        guess_tries: 6,
+        guess_letters: 5,
+    };
+
+    let mut result = GameResult::Failure;
+
+    // Array of guessed letters in order from A to Z, and the number of instances of the letter in the target word
+    let mut target_letter_count: std::collections::HashMap<char, i8> =
+        std::collections::HashMap::new();
+
+    // Possible words to be the target word to guess
+    /*let possible_words = [
+        "gamer", "silly", "fucky", "death", "ocher", "knife", "trans", "music",
+    ];*/
+
+    let possible_words = ["lilac"];
+
+    // The word the player is trying to guess
+    let target_word = possible_words
+        .choose(&mut rand::thread_rng())
+        .unwrap()
+        .to_string();
+
+    // Number of guesses made by the player during the game
+    let mut guesses = 0;
+
+    // Game loop (break on max guesses)
+    while guesses < config.guess_tries {
+        guesses += 1;
+
+        // Number of times a certain letter has been printed as contained in the target word
+        let mut letter_count: std::collections::HashMap<char, u8> =
+            std::collections::HashMap::new();
+
+        // Prompt user input
+        print!("> ");
+        flush();
+
+        // Read user input
+        let mut input: String = text_io::read!("{}\n");
+
+        // Strip user input of additional whitespace, including newlines
+        input = input.trim().to_string();
+
+        // Catch incorrect number of letters in guess, and refund the guess try
+        if input.len() != config.guess_letters as usize {
+            guesses -= 1;
+            println!("The word is {} letters in length.", config.guess_letters);
+            continue;
+        }
+
+        // TODO: This needs to be modified to make the correct place calculations take place BEFORE the letter exists calculations.
+        for i in 0..input.chars().count() {
+            let letter = input.as_bytes()[i] as char;
+
+            // Count letters in input
+            let count = target_word.chars().filter(|c| c == &letter).count() as i8;
+            target_letter_count.entry(letter).or_insert(count);
+
+            if letter == target_word.as_bytes()[i] as char {
+                // If letter is in the correct position
+                *letter_count.entry(letter).or_insert(0) += 1;
+                print!("{}", String::from(letter).green())
+            } else if target_word.chars().any(|c| c == letter) {
+                // If letter exists in target word
+                if *letter_count.entry(letter).or_insert(0)
+                    < target_word.chars().filter(|c| c == &letter).count() as u8
+                {
+                    // Letter has not already been marked as existing (if there are more than one)
+                    *letter_count.entry(letter).or_insert(0) += 1;
+                    print!("{}", String::from(letter).blue());
+                } else {
+                    // Letter has already been marked as existing (if there are more than one)
+                    print!("{}", letter);
+                }
+            } else {
+                // If letter is not in target word
+                print!("{}", letter);
+            }
+        }
+        println!();
+    }
+
+    // Finalize the game
+    println!();
+    match result {
+        GameResult::Success => {}
+        GameResult::Failure => {
+            println!("The word was: {}", target_word)
+        }
+    }
+}
+
+fn flush() {
+    std::io::Write::flush(&mut std::io::stdout()).unwrap();
+}
